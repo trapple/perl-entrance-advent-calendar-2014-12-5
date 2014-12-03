66f461e6235b78acc7be32fa26288b4609fd6854
trapple <trapplejp@gmail.com>
2014-12-03 17:55:04 +0900

    曜日を返すプログラム

---
+++ youbi.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+use utf8;
+binmode STDOUT, 'utf8';
+
+chomp(my $input = <STDIN>);
+if( my $youbi_j = translate($input) ) {
+  print "$input は $youbi_j です\n";
+}else{
+  print "変換できませんでした\n";
+}
+
+sub translate {
+  my $str = shift;
+  my %youbi = (
+    mon => '月曜日',
+    tue => '火曜日',
+    wed => '水曜日',
+    thu => '木曜日',
+    fri => '金曜日',
+    sat => '土曜日',
+    sun => '日曜日',
+  );
+
+  if( exists $youbi{$str} ){
+    return $youbi{$str};
+  }else{
+    return;
+  }
+}
